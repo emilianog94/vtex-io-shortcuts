39b20719112f3ca6109239ac3aba6061558bca25
Emi <emiliano@gioia.com.ar>
2022-03-18 02:31:16 -0300

    API para leer orderForm en checkout (falta manejador popup)

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 // Listener para escuchar cambios en la URL actual
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url) {
         chrome.tabs.sendMessage(tabId, {
--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,27 @@
 handleCheckoutWidget();
+
+const header = document.querySelector('.container.header');
+header.insertAdjacentHTML('beforeend','<pre id="json-renderer"></pre>');
+
+const localStorageOrderForm = JSON.parse(localStorage.getItem('orderform'));
+const orderFormId = localStorageOrderForm.id
+
+
+const options = {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json', Accept: 'application/json'}
+};
+
+fetch(`/api/checkout/pub/orderForm/${orderFormId}`, options)
+.then(response => response.json())
+.then(response => {
+   console.log("la respuesta es");
+   console.log(response);
+   $('#json-renderer').jsonViewer(response);
+
+})
+.catch(err => console.error(err));
+
+
+
+
